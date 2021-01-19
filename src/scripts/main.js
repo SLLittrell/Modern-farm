2e8befc5ba5148ca13f8f465a1a51d6be24d2dc9
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-18 18:24:34 -0600

    Created seed functions in seed .js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,14 @@
 console.log("Welcome to the main module")
 
+import {createPlan} from './plan.js'
+import {createCorn} from './seeds/corn.js'
+import { createAsparagus } from "./seeds/asparagus.js"
+
+createPlan()
+const yearlyPlan = createPlan()
+console.log(yearlyPlan)
+
+
+
+const asparagusSeed = createAsparagus()
+console.log(asparagusSeed)
