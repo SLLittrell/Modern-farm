f4c927e82bd9672924c5ae49e4050d8e42a20ff6
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-18 19:11:31 -0600

    tilled the Field seed function in field.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@ console.log("Welcome to the main module")
 import {createPlan} from './plan.js'
 import {createCorn} from './seeds/corn.js'
 import { createAsparagus } from "./seeds/asparagus.js"
+import {usePlants} from './field.js'
 
 createPlan()
 const yearlyPlan = createPlan()
@@ -12,3 +13,5 @@ console.log(yearlyPlan)
 
 const asparagusSeed = createAsparagus()
 console.log(asparagusSeed)
+
+console.log("fieldArray", usePlants())
